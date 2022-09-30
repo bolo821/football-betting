418d03422250135792706c03964f6dc80f13004e
fisher821 <glorybolo@outlook.com>
2022-09-30 15:29:32 +0300

    update

---
+++ frontend/src/components/sections/matchData.js
@@ -0,0 +1,85 @@
+import ArgentinaImg from '../../assets/images/teams/ar.png';
+import AustraliaImg from '../../assets/images/teams/au.png';
+import BelgiumImg from '../../assets/images/teams/be.png';
+import Brazilimg from '../../assets/images/teams/br.png';
+import CameroonImg from '../../assets/images/teams/cm.png';
+import CanadaImg from '../../assets/images/teams/ca.png';
+import Costa_RicaImg from '../../assets/images/teams/cr.png';
+import CroatiaImg from '../../assets/images/teams/hr.png';
+import DenmarkImg from '../../assets/images/teams/dk.png';
+import EnglandImg from '../../assets/images/teams/gb-eng.png';
+import EcuadorImg from '../../assets/images/teams/ec.png';
+import FranceImg from '../../assets/images/teams/fr.png';
+import GermanyImg from '../../assets/images/teams/de.png';
+import GhanaImg from '../../assets/images/teams/gh.png';
+import IranImg from '../../assets/images/teams/ir.png';
+import JapanImg from '../../assets/images/teams/jp.png';
+import KrImg from '../../assets/images/teams/kr.png';
+import MexicoImg from '../../assets/images/teams/mx.png';
+import MoroccoImg from '../../assets/images/teams/ma.png';
+import NetherlandsImg from '../../assets/images/teams/nl.png';
+import PolandImg from '../../assets/images/teams/pl.png';
+import PortugalImg from '../../assets/images/teams/pt.png';
+import QatarImg from '../../assets/images/teams/qa.jpeg';
+import Saudi_ArabiaImg from '../../assets/images/teams/sa.png';
+import SenegalImg from '../../assets/images/teams/sn.png';
+import SerbiaImg from '../../assets/images/teams/rs.png';
+import SpainImg from '../../assets/images/teams/es.png';
+import SwitzerlandImg from '../../assets/images/teams/ch.png';
+import TunisiaImg from '../../assets/images/teams/tn.png';
+import UruguayImg from '../../assets/images/teams/uy.png';
+import USAImg from '../../assets/images/teams/us.png';
+import WalesImg from '../../assets/images/teams/gb-wls.png';
+
+export const matchData = [
+    { team1: 'Qatar', team1Logo: QatarImg, team2: 'Ecuador', team2Logo: EcuadorImg, days: '54', hours: '20', mins: '30', secs: '40', time: 'Nov 20 2022, 16:00' },
+    { team1: 'Senegal', team1Logo: SenegalImg, team2: 'Netherlands', team2Logo: NetherlandsImg, days: '54', hours: '20', mins: '30', secs: '40', time: 'Nov 20 2022, 16:00' },
+    { team1: 'England', team1Logo: EnglandImg, team2: 'Iran', team2Logo: IranImg, days: '54', hours: '20', mins: '30', secs: '40', time: 'Nov 21 2022, 13:00' },
+    { team1: 'USA', team1Logo: USAImg, team2: 'Wales', team2Logo: WalesImg, days: '54', hours: '20', mins: '30', secs: '40', time: 'Nov 21 2022, 19:00' },
+    { team1: 'Argentina', team1Logo: ArgentinaImg, team2: 'Saudi Arabia', team2Logo: Saudi_ArabiaImg, days: '54', hours: '20', mins: '30', secs: '40', time: 'Nov 22 2022, 10:00' },
+    { team1: 'Mexico', team1Logo: MexicoImg, team2: 'Poland', team2Logo: PolandImg, days: '54', hours: '20', mins: '30', secs: '40', time: 'Nov 22 2022, 16:00' },
+    { team1: 'Denmark', team1Logo: DenmarkImg, team2: 'Tunisia', team2Logo: TunisiaImg, days: '54', hours: '20', mins: '30', secs: '40', time: 'Nov 22 2022, 13:00' },
+    { team1: 'France', team1Logo: FranceImg, team2: 'Australia', team2Logo: AustraliaImg, days: '54', hours: '20', mins: '30', secs: '40', time: 'Nov 22 2022, 19:00' },
+    { team1: 'Germany', team1Logo: GermanyImg, team2: 'Japan', team2Logo: JapanImg, days: '54', hours: '20', mins: '30', secs: '40', time: 'Nov 23 2022, 13:00' },
+    { team1: 'Spain', team1Logo: SpainImg, team2: 'Costa Rica', team2Logo: Costa_RicaImg, days: '54', hours: '20', mins: '30', secs: '40', time: 'Nov 23 2022, 16:00' },
+    { team1: 'Morocco', team1Logo: MoroccoImg, team2: 'Croatia', team2Logo: CroatiaImg, days: '54', hours: '20', mins: '30', secs: '40', time: 'Nov 23 2022, 10:00' },
+    { team1: 'Belgium', team1Logo: BelgiumImg, team2: 'Canada', team2Logo: CanadaImg, days: '54', hours: '20', mins: '30', secs: '40', time: 'Nov 23 2022, 19:00' },
+    { team1: 'Switzerland', team1Logo: SwitzerlandImg, team2: 'Cameroon', team2Logo: CameroonImg, days: '54', hours: '20', mins: '30', secs: '40', time: 'Nov 24 2022, 10:00' },
+    { team1: 'Brazil', team1Logo: Brazilimg, team2: 'Serbia', team2Logo: SerbiaImg, days: '54', hours: '20', mins: '30', secs: '40', time: 'Nov 24 2022, 19:00' },
+    { team1: 'Uruguay', team1Logo: UruguayImg, team2: 'South Korea', team2Logo: KrImg, days: '54', hours: '20', mins: '30', secs: '40', time: 'Nov 24 2022, 13:00' },
+    { team1: 'Portugal', team1Logo: PortugalImg, team2: 'Ghana', team2Logo: GhanaImg, days: '54', hours: '20', mins: '30', secs: '40', time: 'Nov 24 2022, 16:00' },
+    
+    { team1: 'Qatar', team1Logo: QatarImg, team2: 'Senegal', team2Logo: SenegalImg, days: '54', hours: '20', mins: '30', secs: '40', time: 'Nov 25 2022, 13:00' },
+    { team1: 'Netherlands', team1Logo: NetherlandsImg, team2: 'Ecuador', team2Logo: EcuadorImg, days: '54', hours: '20', mins: '30', secs: '40', time: 'Nov 25 2022, 16:00' },
+    { team1: 'Wales', team1Logo: WalesImg, team2: 'Iran', team2Logo: IranImg, days: '54', hours: '20', mins: '30', secs: '40', time: 'Nov 25 2022, 10:00' },
+    { team1: 'England', team1Logo: EnglandImg, team2: 'USA', team2Logo: USAImg, days: '54', hours: '20', mins: '30', secs: '40', time: 'Nov 25 2022, 19:00' },
+    { team1: 'Poland', team1Logo: PolandImg, team2: 'Saudi Arabia', team2Logo: Saudi_ArabiaImg, days: '54', hours: '20', mins: '30', secs: '40', time: 'Nov 26 2022, 13:00' },
+    { team1: 'Argentina', team1Logo: ArgentinaImg, team2: 'Mexico', team2Logo: MexicoImg, days: '54', hours: '20', mins: '30', secs: '40', time: 'Nov 26 2022, 19:00' },
+    { team1: 'Tunisia', team1Logo: TunisiaImg, team2: 'Australia', team2Logo: AustraliaImg, days: '54', hours: '20', mins: '30', secs: '40', time: 'Nov 26 2022, 10:00' },
+    { team1: 'France', team1Logo: FranceImg, team2: 'Denmark', team2Logo: DenmarkImg, days: '54', hours: '20', mins: '30', secs: '40', time: 'Nov 26 2022, 16:00' },
+    { team1: 'Japan', team1Logo: JapanImg, team2: 'Costa Rica', team2Logo: Costa_RicaImg, days: '54', hours: '20', mins: '30', secs: '40', time: 'Nov 27 2022, 10:00' },
+    { team1: 'Spain', team1Logo: SpainImg, team2: 'Germany', team2Logo: GermanyImg, days: '54', hours: '20', mins: '30', secs: '40', time: 'Nov 27 2022, 19:00' },
+    { team1: 'Croatia', team1Logo: CroatiaImg, team2: 'Canada', team2Logo: CanadaImg, days: '54', hours: '20', mins: '30', secs: '40', time: 'Nov 27 2022, 16:00' },
+    { team1: 'Belgium', team1Logo: BelgiumImg, team2: 'Morocco', team2Logo: MoroccoImg, days: '54', hours: '20', mins: '30', secs: '40', time: 'Nov 27 2022, 13:00' },
+    { team1: 'Cameroon', team1Logo: CameroonImg, team2: 'Serbia', team2Logo: SerbiaImg, days: '54', hours: '20', mins: '30', secs: '40', time: 'Nov 28 2022, 10:00' },
+    { team1: 'Brazil', team1Logo: Brazilimg, team2: 'Switzerland', team2Logo: SwitzerlandImg, days: '54', hours: '20', mins: '30', secs: '40', time: 'Nov 28 2022, 16:00' },
+    { team1: 'South Korea', team1Logo: KrImg, team2: 'Ghana', team2Logo: GhanaImg, days: '54', hours: '20', mins: '30', secs: '40', time: 'Nov 28 2022, 13:00' },
+    { team1: 'Portugal', team1Logo: PortugalImg, team2: 'Uruguay', team2Logo: UruguayImg, days: '54', hours: '20', mins: '30', secs: '40', time: 'Nov 28 2022, 19:00' },
+
+    { team1: 'Ecuador', team1Logo: EcuadorImg, team2: 'Senegal', team2Logo: SenegalImg, days: '54', hours: '20', mins: '30', secs: '40', time: 'Nov 29 2022, 15:00' },
+    { team1: 'Netherlands', team1Logo: NetherlandsImg, team2: 'Qatar', team2Logo: QatarImg, days: '54', hours: '20', mins: '30', secs: '40', time: 'Nov 29 2022, 15:00' },
+    { team1: 'Iran', team1Logo: IranImg, team2: 'USA', team2Logo: USAImg, days: '54', hours: '20', mins: '30', secs: '40', time: 'Nov 29 2022, 19:00' },
+    { team1: 'Wales', team1Logo: WalesImg, team2: 'England', team2Logo: EnglandImg, days: '54', hours: '20', mins: '30', secs: '40', time: 'Nov 29 2022, 19:00' },
+    { team1: 'Poland', team1Logo: PolandImg, team2: 'Argentina', team2Logo: ArgentinaImg, days: '54', hours: '20', mins: '30', secs: '40', time: 'Nov 30 2022, 19:00' },
+    { team1: 'Saudi Arabia', team1Logo: Saudi_ArabiaImg, team2: 'Mexico', team2Logo: MexicoImg, days: '54', hours: '20', mins: '30', secs: '40', time: 'Nov 30 2022, 19:00' },
+    { team1: 'Tunisia', team1Logo: TunisiaImg, team2: 'France', team2Logo: FranceImg, days: '54', hours: '20', mins: '30', secs: '40', time: 'Nov 30 2022, 15:00' },
+    { team1: 'Australia', team1Logo: AustraliaImg, team2: 'Denmark', team2Logo: DenmarkImg, days: '54', hours: '20', mins: '30', secs: '40', time: 'Nov 30 2022, 15:00' },
+    { team1: 'Japan', team1Logo: JapanImg, team2: 'Spain', team2Logo: SpainImg, days: '54', hours: '20', mins: '30', secs: '40', time: 'Dec 1 2022, 19:00' },
+    { team1: 'Costa Rica', team1Logo: Costa_RicaImg, team2: 'Germany', team2Logo: GermanyImg, days: '54', hours: '20', mins: '30', secs: '40', time: 'Dec 1 2022, 19:00' },
+    { team1: 'Croatia', team1Logo: CroatiaImg, team2: 'Belgium', team2Logo: BelgiumImg, days: '54', hours: '20', mins: '30', secs: '40', time: 'Dec 1 2022, 15:00' },
+    { team1: 'Canada', team1Logo: CanadaImg, team2: 'Morocco', team2Logo: MoroccoImg, days: '54', hours: '20', mins: '30', secs: '40', time: 'Dec 1 2022, 15:00' },
+    { team1: 'Serbia', team1Logo: SerbiaImg, team2: 'Switzerland', team2Logo: SwitzerlandImg, days: '54', hours: '20', mins: '30', secs: '40', time: 'Dec 2 2022, 22:00' },
+    { team1: 'Cameroon', team1Logo: CameroonImg, team2: 'Brazil', team2Logo: Brazilimg, days: '54', hours: '20', mins: '30', secs: '40', time: 'Dec 2 2022, 19:00' },
+    { team1: 'South Korea', team1Logo: KrImg, team2: 'Portugal', team2Logo: PortugalImg, days: '54', hours: '20', mins: '30', secs: '40', time: 'Dec 2 2022, 15:00' },
+    { team1: 'Ghana', team1Logo: GhanaImg, team2: 'Uruguay', team2Logo: UruguayImg, days: '54', hours: '20', mins: '30', secs: '40', time: 'Dec 2 2022, 15:00' },
+];
