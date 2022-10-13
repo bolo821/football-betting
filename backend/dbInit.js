eb9018be30aa4bc4b49b261174af431bd75e43f5
fisher821 <glorybolo@outlook.com>
2022-10-13 17:49:14 +0300

    update

--- backend/dbInit.js
+++ backend/dbInit.js
@@ -4,6 +4,6 @@ const db_string = process.env.MONGO_URL;
 require('./models/Match');
 require("./models/Token");
 
-mongoose.connect(db_string, { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true }).then(() => {
+mongoose.connect(db_string, { user: process.env.MONGO_USERNAME, pass: process.env.MONGO_PASSWORD, useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true }).then(() => {
   	console.log("MongoDB connected...");
 }).catch(err => console.log(err));
