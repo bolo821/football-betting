f5176a7a4ba3dbc487f8802494bebd2b551692df
fisher821 <glorybolo@outlook.com>
2022-10-17 05:22:08 +0300

    Update history page

--- frontend/src/pages/History.js
+++ frontend/src/pages/History.js
@@ -6,14 +6,13 @@ import Section1 from '../components/sections/Section1';
 import HistoryTable from '../components/history/HistoryTable';
 import { getRoundedNumber } from '../utils/helper';
 
-import { worldcupMatchData, uefaMatchData } from '../components/sections/matchData';
-
 const History = () => {
     const betStatus = useSelector(state => state.transaction.betStatus);
     const claimHistory = useSelector(state => state.transaction.claimHistory);
     const earnings = useSelector(state => state.transaction.earnings);
     const betAmounts = useSelector(state => state.transaction.betAmounts);
     const betResult = useSelector(state => state.transaction.betResult);
+    const matches = useSelector(state => state.match.matches);
 
     const [data, setData] = useState([]);
 
@@ -21,106 +20,11 @@ const History = () => {
         let temp = [];
 
         if (betStatus.length) {
-            for (let i=0; i<worldcupMatchData.length; i++) {
-                if (worldcupMatchData[i].id >= betStatus.length) continue;
-                let betId = worldcupMatchData[i].id;
+            for (let i=0; i<matches.length; i++) {
+                let betId = matches[i].matchId;
                 let bet = betAmounts[betId];
-                let team1 = worldcupMatchData[i].team1;
-                let team2 = worldcupMatchData[i].team2;
-                let status = betStatus[betId];
-                let ele = {
-                    team1: team1,
-                    team2: team2,
-                }
-                let result = betResult[betId];
-                let status_ = status === 0 ? 'Betting' : 'Reviewing';
-
-                if (bet.win !== '0') {
-                    let profit;
-                    if (status === 0 || status === 1) {
-                        profit = earnings[betId].win;
-                    } else {
-                        if (result === 0) {
-                            if (earnings[betId].win === '0') {
-                                profit = claimHistory[betId];
-                                status_ = 'Claimed';
-                            } else {
-                                profit = earnings[betId].win;
-                                status_ = 'Clamable';
-                            }
-                        } else {
-                            profit = '0';
-                            status_ = 'Lost';
-                        }
-                    }
-                    temp.push({
-                        ...ele,
-                        choice: team1,
-                        betAmount: bet.win,
-                        profit: getRoundedNumber(profit),
-                        status: status_,
-                    });
-                }
-                if (bet.draw !== '0') {
-                    let profit;
-                    if (status === 0 || status === 1) {
-                        profit = earnings[betId].draw;
-                    } else {
-                        if (result === 1) {
-                            if (earnings[betId].draw === '0') {
-                                profit = claimHistory[betId];
-                                status_ = 'Claimed';
-                            } else {
-                                profit = earnings[betId].draw;
-                                status_ = 'Clamable';
-                            }
-                        } else {
-                            profit = '0';
-                            status_ = 'Lost';
-                        }
-                    }
-                    temp.push({
-                        ...ele,
-                        choice: 'Draw',
-                        betAmount: bet.draw,
-                        profit: getRoundedNumber(profit),
-                        status: status_,
-                    });
-                }
-                if (bet.lose !== '0') {
-                    let profit;
-                    if (status === 0 || status === 1) {
-                        profit = earnings[betId].lose;
-                    } else {
-                        if (result === 2) {
-                            if (earnings[betId].lose === '0') {
-                                profit = claimHistory[betId];
-                                status_ = 'Claimed';
-                            } else {
-                                profit = earnings[betId].lose;
-                                status_ = 'Clamable';
-                            }
-                        } else {
-                            profit = '0';
-                            status_ = 'Lost';
-                        }
-                    }
-                    temp.push({
-                        ...ele,
-                        choice: team2,
-                        betAmount: bet.lose,
-                        profit: getRoundedNumber(profit),
-                        status: status_,
-                    });
-                }
-            }
-
-            for (let i=0; i<uefaMatchData.length; i++) {
-                if (uefaMatchData[i].id >= betStatus.length) continue;
-                let betId = uefaMatchData[i].id;
-                let bet = betAmounts[betId];
-                let team1 = uefaMatchData[i].team1;
-                let team2 = uefaMatchData[i].team2;
+                let team1 = matches[i].team1Name;
+                let team2 = matches[i].team2Name;
                 let status = betStatus[betId];
                 let ele = {
                     team1: team1,
@@ -211,7 +115,7 @@ const History = () => {
         }
 
         setData(temp);
-    }, [betStatus]);
+    }, [betStatus, matches]);
 
     return (
         <>
