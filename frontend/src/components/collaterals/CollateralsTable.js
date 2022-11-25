5f34bf1c92beaafc8417663469f51c436a743036
fisher821 <glorybolo@outlook.com>
2022-11-25 04:31:17 +0300

    Update table overflows in mobile view

--- frontend/src/components/collaterals/CollateralsTable.js
+++ frontend/src/components/collaterals/CollateralsTable.js
@@ -32,51 +32,53 @@ const CollateralsTable = props => {
         <div className="container">
             { data.length > 0 ?
                 <div className="table-responsive">
-                    <table className="table history-table-rt">
-                        <thead>
-                            <tr>
-                                <th scope="col">Crypto</th>
-                                <th scope="col">Amount</th>
-                                <th scope="col">Deposit</th>
-                                <th scope="col">Withdraw</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            { data.map((ele, index) => (
-                                <tr key={index}>
-                                    <td>{ele.crypto}</td>
-                                    <td>{parseInt(ele.amount * 1000) / 1000}</td>
-                                    <td>
-                                        { index !== 0 && ele.allowance === 0 ?
-                                            <button
-                                                type="button"
-                                                className="cmn-btn reg set-bn-rt"
-                                                onClick={ele.handleApprove}
-                                            >
-                                                Approve
-                                            </button> :
-                                            <button
-                                                type="button"
-                                                className="cmn-btn reg set-bn-rt"
-                                                onClick={() => {handleDeposit(ele, index)}}
-                                            >
-                                                Deposit
-                                            </button>
-                                        }
-                                    </td>
-                                    <td>
-                                        <button
-                                            type="button"
-                                            className="cmn-btn reg set-bn-rt"
-                                            onClick={() => {handleWithdraw(ele, index)}}
-                                        >
-                                            Withdraw
-                                        </button>
-                                    </td>
+                    <div className='overflow-auto mb-3'>
+                        <table className="table history-table-rt">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Crypto</th>
+                                    <th scope="col">Amount</th>
+                                    <th scope="col">Deposit</th>
+                                    <th scope="col">Withdraw</th>
                                 </tr>
-                            ))}
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                                { data.map((ele, index) => (
+                                    <tr key={index}>
+                                        <td>{ele.crypto}</td>
+                                        <td>{parseInt(ele.amount * 1000) / 1000}</td>
+                                        <td>
+                                            { index !== 0 && ele.allowance === 0 ?
+                                                <button
+                                                    type="button"
+                                                    className="cmn-btn reg set-bn-rt"
+                                                    onClick={ele.handleApprove}
+                                                >
+                                                    Approve
+                                                </button> :
+                                                <button
+                                                    type="button"
+                                                    className="cmn-btn reg set-bn-rt"
+                                                    onClick={() => {handleDeposit(ele, index)}}
+                                                >
+                                                    Deposit
+                                                </button>
+                                            }
+                                        </td>
+                                        <td>
+                                            <button
+                                                type="button"
+                                                className="cmn-btn reg set-bn-rt"
+                                                onClick={() => {handleWithdraw(ele, index)}}
+                                            >
+                                                Withdraw
+                                            </button>
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
                 </div> :
                 <div className="d-flex w-100 justify-content-center bottom-area mt-60 mb-5">
                     <h5>No collateral to display.</h5>
--- frontend/src/components/history/HistoryTable.js
+++ frontend/src/components/history/HistoryTable.js
@@ -20,30 +20,32 @@ const HistoryTable = props => {
         <div className="container">
             { data.length > 0 ?
                 <div className="table-responsive mb-5">
-                    <table className="table history-table-rt">
-                        <thead>
-                            <tr>
-                                <th scope="col">Match</th>
-                                <th scope="col">Choice</th>
-                                <th scope="col">Status</th>
-                                <th scope="col">Bet Amount</th>
-                                <th scope="col">Reward</th>
-                                <th scope="col">Token</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            { tableData.map((ele, index) => (
-                                <tr key={index}>
-                                    <td>{ele.team1} / {ele.team2}</td>
-                                    <td>{ele.choice}</td>
-                                    <td>{ele.status}</td>
-                                    <td>{ele.betAmount}</td>
-                                    <td>{ele.profit}</td>
-                                    <td>{ele.token}</td>
+                    <div className='overflow-auto mb-3'>
+                        <table className="table history-table-rt">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Match</th>
+                                    <th scope="col">Choice</th>
+                                    <th scope="col">Status</th>
+                                    <th scope="col">Bet Amount</th>
+                                    <th scope="col">Reward</th>
+                                    <th scope="col">Token</th>
                                 </tr>
-                            ))}
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                                { tableData.map((ele, index) => (
+                                    <tr key={index}>
+                                        <td>{ele.team1} / {ele.team2}</td>
+                                        <td>{ele.choice}</td>
+                                        <td>{ele.status}</td>
+                                        <td>{ele.betAmount}</td>
+                                        <td>{ele.profit}</td>
+                                        <td>{ele.token}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
                     <Stack direction="row" justifyContent="flex-end">
                         <Pagination currentPage={currentPage} setCurrentPage={setCurrentPage} totalCount={data.length} pageSize={10} />
                     </Stack>
