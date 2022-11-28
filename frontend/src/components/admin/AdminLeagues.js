9f8e4da9b351e2cd3c4a12b7bd64126f76c69cbb
fisher821 <glorybolo@outlook.com>
2022-11-28 14:58:09 +0300

    refresh

--- frontend/src/components/admin/AdminLeagues.js
+++ frontend/src/components/admin/AdminLeagues.js
@@ -143,14 +143,14 @@ const Leagues = () => {
                         <i className="fas fa-plus"></i>
                         {' '} Add a new event
                     </button>
-                    {/* <button
+                    <button
                         type="button"
                         className="cmn-btn reg connect-bn-rt mb-2"
                         onClick={() => SOCKET.emit('REFRESH')}
                         style={{width: 'fit-content', marginLeft: '5px'}}
                     >
                         Force Refresh
-                    </button> */}
+                    </button>
                 </div>
                 <div className="container">
                     <div className="row">
